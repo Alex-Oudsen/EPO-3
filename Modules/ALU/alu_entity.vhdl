543f02a3b4bd197b3fdbccc267afed1ec41187ec
Alex-Oudsen <ali.x1306@gmail.com>
2014-11-17 10:47:15 +0100

    Create alu_entity.vhdl

---
+++ Modules/ALU/alu_entity.vhdl
@@ -0,0 +1,11 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity alu is
+	  port (clk : in  std_logic;
+		a   : in  std_logic_vector (3 downto 0);
+		b   : in  std_logic_vector (3 downto 0);
+		f   : in  std_logic_vector (2 downto 0);
+		r   : out std_logic_vector (3 downto 0);
+		c   : out std_logic);
+end entity alu;
