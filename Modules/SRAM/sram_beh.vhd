442933e812424db58593a1a42995e713a0b691c5
RensHam <renshamburger@gmail.com>
2014-11-26 14:51:28 +0100

    De timing problemen verholpen nu een net fsm oeps

--- Modules/SRAM/sram_beh.vhd
+++ Modules/SRAM/sram_beh.vhd
@@ -4,45 +4,57 @@ use IEEE.Numeric_Std.all;
 
 
 architecture behaviour of sram is
-type mem_array is array (0 to (2**address'length) -1) of std_logic_vector((write_data'length -1) downto 0);--Het ram element definiëren als een array van de adreslengte bij de grootte van het te schrijven woord
-type memory_state is (read_memory,write_memory,state_reset);					--Het definiëren van de te gebruiken states zodat er een fsm gemaakt kan worden
+type mem_array is array (0 to (2**address'length) -1) of std_logic_vector((write_data'length -1) downto 0);--Het ram element definiï¿½ren als een array van de adreslengte bij de grootte van het te schrijven woord
+type memory_state is (steady,state_reset);					--Het definiï¿½ren van de te gebruiken states zodat er een fsm gemaakt kan worden
 signal ram : mem_array;
 signal new_ram : mem_array;
 signal state,new_state : memory_state;
+--signal data_out : std_logic_vector(3 downto 0);
 begin
 	sram : process(clk)						--Bepalen van de state
 	begin 
 		if rising_edge(clk) then
-			case read_write_line is                  
-				when '1' =>
-					state <= read_memory;     
-				when '0' =>
-		     			state <= write_memory;
-	        		when others =>
-	            			state <= read_memory;
-	    		end case;
 			if (reset = '1') then
 				state <= state_reset;
+			else
+				ram <= new_ram;
+				state <= new_state;
+				--read_data <= data_out;
 			end if;
       		end if;
 	end process sram;
 	sram_state : process(state,clk,read_write_line,write_data)	--Het uit voeren van de opdrachten binnen de states
 	begin
-		if rising_edge(clk) then
-			case state is
-				when read_memory =>			--Lezen van data
+		case state is                  
+			when steady =>
+				if read_write_line ='1' then 		--Lezen van data
 					read_data <= ram(to_integer(unsigned(address)));
-				when write_memory =>			--Data schrijven
-					new_ram(to_integer(unsigned(address))) <= write_data;
+					new_state <= steady;
+					new_ram <= ram;
+				elsif read_write_line = '0' then 	--Data schrijven
+					for J in 0 to 15 loop
+						if (J = to_integer(unsigned(address))) then
+							new_ram(J) <= write_data;
+						else
+							new_ram(J) <= ram(J);
+						end if;
+					end loop;
 					read_data <= "0000";
-				when state_reset =>			-- Alles initialiseren en dus op 0 zetten
-					new_ram <= (others => (others => '0')); 
-					read_data <= "0000";
-			end case;
-			ram<=new_ram;					-- Het wegschrijven van het nieuwe ram
-		end if;
+					new_state <= steady;
+				end if;
+ 			when state_reset =>		-- Alles initialiseren en dus op 0 zetten 
+				new_ram <= (others => (others => '0')); 
+				read_data <= "0000";
+				new_state <= steady;
+	    end case;
 	end process sram_state;
 end behaviour;
+
+
+
+
+
+
 
 
 
