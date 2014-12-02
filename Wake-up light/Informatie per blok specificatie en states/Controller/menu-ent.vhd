872aa1569281e32de6dd5a5002691688dd098bad
RensHam <renshamburger@gmail.com>
2014-12-02 18:24:19 +0100

    Het aanpassen van de menu entity zodat deze gelijk de beeld uitgang doorstuurt aan de lcd blok

--- Wake-up light/Informatie per blok specificatie en states/Controller/menu-ent.vhd
+++ Wake-up light/Informatie per blok specificatie en states/Controller/menu-ent.vhd
@@ -5,14 +5,14 @@ entity menu is
    port(clk			:in    std_logic;
         reset		:in    std_logic;
 		knoppen		:in    std_logic_vector(3 downto 0);
-		wekminuten	:in    std_logic_vector(5 downto 0);
-		wekuren		:in	 std_logic_vector(4 downto 0);
-		wekker_geh	:in	 std_logic;
-		geluid_led	:in   std_logic_vector(1 downto 0)
+		tijd		:in    std_logic_vector(10 downto 0);
+		wektijd		:in	   std_logic_vector(10 downto 0);
+		wekker_geh	:in	   std_logic;
+		geluid_led	:in    std_logic_vector(1 downto 0);
         wektijdmin	:out   std_logic_vector(5 downto 0);
 		wektijduren	:out   std_logic_vector(4 downto 0);
         wekker		:out   std_logic;
-        select_bit	:out   std_logic;
+        beeld		:out   std_logic_vector(11 downto 0);
         data_out	:out   std_logic_vector(1 downto 0));
 end menu;
 
