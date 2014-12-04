68c2e50d55aadca5522502afabc98c9e276fc3f2
MasterKeffer <gkh998@gmail.com>
2014-12-04 10:37:24 +0100

    updated menu entity aan de hand van de ingangs signalen van de lcd

--- Wake-up light/Informatie per blok specificatie en states/Controller/menu-ent.vhd
+++ Wake-up light/Informatie per blok specificatie en states/Controller/menu-ent.vhd
@@ -2,13 +2,17 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity menu is
-   port(clk			:in    std_logic;
+   port(clk				:in    std_logic;
         reset		:in    std_logic;
 		knoppen		:in    std_logic_vector(3 downto 0);
-		tijd		:in    std_logic_vector(10 downto 0);
 		wekdata		:in	   std_logic_vector(13 downto 0);
-		enable		:out   std_logic;
-		wekker		:out   std_logic_vector(13 downto 0);
+		enable			:out   std_logic;
+		wekker			:out		 std_logic_vector(13 downto 0);
+		toggle			:out		 std_logic;
+		menu			:out		 std_logic_vector(2 downto 0));
 end menu;
+
+
+
 
 
