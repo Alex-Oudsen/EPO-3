ff75608582ef432fa8c716283876c2302b0dfd8f
MasterKeffer <gkh998@gmail.com>
2014-12-04 11:45:10 +0100

    Controller fsm opzet (optellen en aftrekken van minuten en uren ontbreekt)

--- Wake-up light/VHDL/controller/menu-ent.vhd
+++ Wake-up light/VHDL/controller/menu-ent.vhd
@@ -2,14 +2,17 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity menu is
-   port(clk       :in    std_logic;
-        reset     :in    std_logic;
-        knoppen   :in    std_logic_vector(3 downto 0);
-        wekdata_in:in    std_logic_vector(13 downto 0);
-        wektijd   :out   std_logic_vector(10 downto 0);
-        wekker    :out   std_logic;
-        select_bit:out   std_logic;
-        data_out  :out   std_logic_vector(1 downto 0));
+   port(clk				:in    std_logic;
+        reset		:in    std_logic;
+		knoppen		:in    std_logic_vector(3 downto 0);
+		wekdata		:in	   std_logic_vector(13 downto 0);
+		enable			:out   std_logic;
+		wekker			:out		 std_logic_vector(13 downto 0);
+		toggle			:out		 std_logic;
+		menu			:out		 std_logic_vector(2 downto 0));
 end menu;
+
+
+
 
 
