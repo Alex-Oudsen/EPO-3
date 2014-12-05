1fbfdc9da3c98a1cf37871381c5327d4752a5d54
RensHam <renshamburg@gmail.com>
2014-12-05 11:56:24 +0100

    Fix syntax errors er moeten nog gekeken worden of het goed werkt omtrent timing

--- Wake-up light/VHDL/controller/geheugen-behaviour_fsm.vhd
+++ Wake-up light/VHDL/controller/geheugen-behaviour_fsm.vhd
@@ -2,11 +2,11 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 architecture behaviour of geheugen is
-begin
 type geheugen_state is (read,write,state_reset);
 signal state,new_state : geheugen_state;
-signal wek_temp : std_logic_vector(13 down to 0 );
-assign : process(clk,reset,new_state);
+signal wek_temp : std_logic_vector(13 downto 0 );
+begin
+assign : process(clk,reset,new_state)
 begin
 	if rising_edge(clk) then
 		if reset = '1' then
@@ -15,24 +15,24 @@ begin
 			state <= new_state;
 		end if;
 	end if;
-end process assign
+end process assign;
 
 actie : process(clk,reset,new_state,enable,wek_in)
 begin
 	case state is
-		when reset
-			wek_temp <= "00000000000000";
-			wekout <= wek_temp;
-		when read
-			wekout <= wek_temp;
-		when write
+		when state_reset =>
+			wek_temp <= (others => '0');
+			wek_out <= wek_temp;
+		when read =>
+			wek_out <= wek_temp;
+		when write =>
 			wek_temp <= wek_in;
-			wekout <= wek_temp;
+			wek_out <= wek_temp;
 	end case;
-
-next_state : process(clk,reset,enalbe,wek_in)
+end process actie;
+next_state : process(clk,reset,enable,wek_in)
 begin
-	if enalbe = '1' then
+	if enable = '1' then
 		new_state <= write;
 	else 
 		new_state <= read;
@@ -53,6 +53,9 @@ end process next_state;
 	--	end if;
 --	end if;
 --end process geheugen_element;
---end behaviour;
+end behaviour;
+
+
+
 
 
