f32ed8a63e298a110cafd1477463bc6135611a66
RensHam <renshamburger@gmail.com>
2014-12-05 11:07:07 +0100

    Toevoegen van de componenten in de top entity van de controller

--- Wake-up light/VHDL/controller/controller.vhd
+++ Wake-up light/VHDL/controller/controller.vhd
@@ -10,4 +10,23 @@ entity controller is
         menu   :out   std_logic_vector(2 downto 0));
 end controller;
 
+component menu is
+	port(clk		:in		std_logic;
+		reset		:in		std_logic;
+		knoppen		:in		std_logic_vector(3 downto 0);
+		wekdata		:in		std_logic_vector(13 downto 0);
+		enable		:out	std_logic;
+		wekker		:out	std_logic_vector(13 downto 0);
+		toggle		:out	std_logic;
+		menu		:out	std_logic_vector(2 downto 0));
+end component menu;
+
+component geheugen is
+	port(clk    :in    std_logic;
+        reset  :in    std_logic;
+        enable :in    std_logic;
+        wek_in :in    std_logic_vector(13 downto 0);
+        wek_out:out   std_logic_vector(13 downto 0));
+end component geheugen;
+
 
