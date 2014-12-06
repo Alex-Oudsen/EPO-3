37a47ee621835852a0a3ad837dad71bdd1560b28
Alex <ali.x1306@gmail.com>
2014-12-06 15:36:19 +0100

    Converter van BCD naar binair

---
+++ Wake-up light/VHDL/DCF77/BCD to bin/BCD2bin_entity.vhdl
@@ -0,0 +1,13 @@
+-- Alex Oudsen, 4325494
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity bcd2bin is
+    port (clk:		in  std_logic;
+	  reset:	in  std_logic;
+	  start:	in  std_logic;
+	  bcd_in:	in  std_logic_vector(7 downto 0);
+	  bin_out:	out std_logic_vector(6 downto 0);
+	  ready:	out std_logic);
+end entity bcd2bin;
---
+++ Wake-up light/VHDL/DCF77/BCD to bin/BCD2bin_tb_entity.vhdl
@@ -0,0 +1,7 @@
+-- Alex Oudsen, 4325494
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity bcd2bin_tb is
+end entity bcd2bin_tb;
