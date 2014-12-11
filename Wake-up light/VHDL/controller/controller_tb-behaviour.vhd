6abff2dee645818b35f8ac450626ce10085b53d9
RensHam <renshamburg@gmail.com>
2014-12-11 11:38:28 +0100

    Het toevoegen van de juiste rust signalen zodat zodat de gebruikte buffer werkt

--- Wake-up light/VHDL/controller/controller_tb-behaviour.vhd
+++ Wake-up light/VHDL/controller/controller_tb-behaviour.vhd
@@ -26,49 +26,74 @@ begin
 			'0' after 62 ns;		--knoppen(1) = set
 									--knoppen(2) = up
 	knoppen <=	"0000" after 0 ns,	--knoppen(3) = down
-			"0010" after 68 ns,		--rust -> wekker_toggle
-			"0010" after 108 ns,	--wekker_toggle -> rust
-			"0001" after 148 ns,	--rust -> wekkertijd
-			"0001" after 188 ns,	--wekkertijd -> rust
-			"0001" after 228 ns,	--rust -> wekkertijd
-			"0010" after 268 ns,	--wekkertijd -> uren_set
-			"0100" after 308 ns,	--uren_set -> uren_plus
-			"0000" after 348 ns,	--uren_plus -> uren_set
-			"1000" after 388 ns,	--uren_set -> uren_min
-			"0000" after 428 ns,	--uren_min -> uren_set
-			"0001" after 468 ns,	--uren_set -> rust
-			"0001" after 508 ns,	--rust -> wekkertijd
-			"0010" after 548 ns,	--wekkertijd -> uren_set
-			"0010" after 588 ns,	--uren_set -> minuten_set
-			"0100" after 628 ns,	--minuten_set -> minuten_plus
-			"0000" after 668 ns,	--minuten_plus -> minuten_set
-			"1000" after 708 ns,	--minuten_set -> minuten_min
-			"0000" after 748 ns,	--minuten_min -> minuten_set
-			"0001" after 788 ns,	--minuten_set -> rust
-			"0001" after 828 ns,	--rust -> wekkertijd
-			"0010" after 868 ns,	--wekkertijd -> uren_set
-			"0010" after 908 ns,	--uren_set -> minuten_set
-			"0010" after 948 ns,	--minuten_set -> wekkertijd 	EIGENLIJK GAAT DIT NAAR RUST TOE
-			"0001" after 988 ns,	--rust -> wekkertijd
-			"1000" after 1028 ns,	--wekkertijd -> led
-			"0001" after 1068 ns,	--led -> rust
-			"0001" after 1108 ns,	--rust -> wekkertijd
-			"0100" after 1148 ns,	--wekkertijd -> geluid
-			"0100" after 1188 ns,	--geluid -> led
-			"0100" after 1228 ns,	--led -> wekkertijd
-			"1000" after 1268 ns,	--wekkertijd -> led
-			"1000" after 1308 ns,	--led -> geluid
-			"1000" after 1348 ns,	--geluid -> wekkertijd
-			"1000" after 1388 ns,	--wekkertijd -> led
-			"0010" after 1428 ns,	--led -> led_toggle
-			"0000" after 1468 ns,	--led_toggle -> led
-			"1000" after 1508 ns,	--led -> geluid
-			"0010" after 1548 ns,	--geluid -> geluid_toggle
-			"0000" after 1588 ns,	--geluid_toggle -> geluid
-			"0001" after 1628 ns,	--geluid -> rust
-			"0000" after 1668 ns; 	--done, done, done;			
-
-	--wekdata <= "00000000000000" after 20 ns;
+			"0010" after 68 ns,	--rust -> wekker_toggle
+			"0000" after 108 ns,	--knoppen(3) = down
+			"0010" after 188 ns,	--wekker_toggle -> rust
+			"0000" after 228 ns,	--knoppen(3) = down
+			"0001" after 308 ns,	--rust -> wekkertijd
+			"0000" after 348 ns,	--knoppen(3) = down
+			"0001" after 428 ns,	--wekkertijd -> rust
+			"0000" after 468 ns,	--knoppen(3) = down
+			"0001" after 548 ns,	--rust -> wekkertijd
+			"0000" after 588 ns,	--knoppen(3) = down
+			"0010" after 668 ns,	--wekkertijd -> uren_set
+			"0000" after 708 ns,	--knoppen(3) = down
+			"0100" after 788 ns,	--uren_set -> uren_plus
+			"0000" after 828 ns,	--knoppen(3) = down
+			"1000" after 908 ns,	--uren_set -> uren_min
+			"0000" after 948 ns,	--uren_min -> uren_set
+			"0001" after 1028 ns,	--uren_set -> rust
+			"0000" after 1068 ns,	--knoppen(3) = down
+			"0001" after 1148 ns,	--rust -> wekkertijd
+			"0000" after 1188 ns,	--knoppen(3) = down
+			"0010" after 1268 ns,	--wekkertijd -> uren_set
+			"0000" after 1308 ns,	--knoppen(3) = down
+			"0010" after 1388 ns,	--uren_set -> minuten_set
+			"0000" after 1428 ns,	--knoppen(3) = down
+			"0100" after 1508 ns,	--minuten_set -> minuten_plus
+			"0000" after 1548 ns,	--minuten_plus -> minuten_set
+			"1000" after 1628 ns,	--minuten_set -> minuten_min
+			"0000" after 1668 ns,	--minuten_min -> minuten_set
+			"0001" after 1748 ns,	--minuten_set -> rust
+			"0000" after 1788 ns,	--knoppen(3) = down
+			"0001" after 1868 ns,	--rust -> wekkertijd
+			"0000" after 1908 ns,	--knoppen(3) = down
+			"0010" after 1988 ns,	--wekkertijd -> uren_set
+			"0000" after 2028 ns,	--knoppen(3) = down
+			"0010" after 2108 ns,	--uren_set -> minuten_set
+			"0000" after 2148 ns,	--knoppen(3) = down
+			"0010" after 2228 ns,	--minuten_set -> wekkertijd 	EIGENLIJK GAAT DIT NAAR RUST TOE
+			"0000" after 2268 ns,	--knoppen(3) = down
+			"0001" after 2348 ns,	--rust -> wekkertijd
+			"0000" after 2388 ns,	--knoppen(3) = down
+			"1000" after 2468 ns,	--wekkertijd -> led
+			"0000" after 2508 ns,	--knoppen(3) = down
+			"0001" after 2588 ns,	--led -> rust
+			"0000" after 2628 ns,	--knoppen(3) = down
+			"0001" after 2708 ns,	--rust -> wekkertijd
+			"0000" after 2748 ns,	--knoppen(3) = down
+			"0100" after 2828 ns,	--wekkertijd -> geluid
+			"0000" after 2968 ns,	--knoppen(3) = down
+			"0100" after 3048 ns,	--geluid -> led
+			"0000" after 3088 ns,	--knoppen(3) = down
+			"0100" after 3168 ns,	--led -> wekkertijd
+			"0000" after 3208 ns,	--knoppen(3) = down
+			"1000" after 3288 ns,	--wekkertijd -> led
+			"0000" after 3328 ns,	--knoppen(3) = down
+			"1000" after 3408 ns,	--led -> geluid
+			"0000" after 3448 ns,	--knoppen(3) = down
+			"1000" after 3528 ns,	--geluid -> wekkertijd
+			"0000" after 3568 ns,	--knoppen(3) = down
+			"1000" after 3648 ns,	--wekkertijd -> led
+			"0000" after 3688 ns,	--knoppen(3) = down
+			"0010" after 3768 ns,	--led -> led_toggle
+			"0000" after 3808 ns,	--led_toggle -> led
+			"1000" after 3888 ns,	--led -> geluid
+			"0000" after 3928 ns,	--knoppen(3) = down
+			"0010" after 4008 ns,	--geluid -> geluid_toggle
+			"0000" after 4048 ns,	--geluid_toggle -> geluid
+			"0001" after 4128 ns,	--geluid -> rust
+			"0000" after 4168 ns; 	--done, done, done;			
 
 	controller_pm: controller port map(clk, reset, knoppen, wekker,menu_signal);
 end architecture;
