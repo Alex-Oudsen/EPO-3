af78f2679ad3cd56b1033f62339180c7ae59df3c
RensHam <renshamburger@gmail.com>
2014-12-15 10:47:25 +0100

    Tesbench aanpassen zodat we de snelheid van het gehuegen kunnen testen

--- Wake-up light/VHDL/controller/geheugen-behaviour-tb.vhd
+++ Wake-up light/VHDL/controller/geheugen-behaviour-tb.vhd
@@ -22,22 +22,36 @@ begin
 			'0' after 85 ns;
 
 
-	enable <='0' after 0 ns, 
-			'1' after 155 ns,
-			'0' after 365 ns,
-			'1' after 665 ns;
+	enable <=	'0' after 0 ns, 
+			'1' after 150 ns,
+			'0' after 290 ns,
+			'1' after 590 ns;
 
-	wek_in <= 	"00000000000000" after 0 ns,
-				"00000000000010" after 65 ns,
-				"00000000000011" after 105 ns,
-				"00000000000100" after 145 ns,
-				"00000000001000" after 385 ns,
-				"00000000010000" after 705 ns,
-				"00000000000000" after 745 ns,
-				"00000000000000" after 765 ns;
+	wek_in <= 	"00000000000001" after 0 ns,
+			"00000000000010" after 70 ns,
+			"00000000000011" after 110 ns,
+			"00000000000100" after 150 ns,
+			"00000000000101" after 190 ns,
+			"00000000000110" after 230 ns,
+			"00000000000111" after 270 ns,
+			"00000000001000" after 310 ns,
+			"00000000001001" after 350 ns,
+			"00000000001010" after 390 ns,
+			"00000000001011" after 430 ns,
+			"00000000001100" after 470 ns,
+			"00000000001101" after 510 ns,
+			"00000000001110" after 550 ns,
+			"00000000001111" after 590 ns,
+			"00000000010000" after 630 ns,
+			"00000000010001" after 680 ns,
+			"00000000010010" after 735 ns,
+			"00000000010111" after 779 ns;
 
 	geheugen_pm: geheugen port map(clk,reset,enable,wek_in,wek_out);
 end behaviour;
+
+
+
 
 
 
