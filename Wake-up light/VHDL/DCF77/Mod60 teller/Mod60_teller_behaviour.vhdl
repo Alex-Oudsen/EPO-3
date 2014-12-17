c96798c1ef5392c4a04ffd55094fbea1e3b78231
Alex <ali.x1306@gmail.com>
2014-12-17 12:48:37 +0100

    Verbetering mod60 teller

--- Wake-up light/VHDL/DCF77/Mod60 teller/Mod60_teller_behaviour.vhdl
+++ Wake-up light/VHDL/DCF77/Mod60 teller/Mod60_teller_behaviour.vhdl
@@ -10,7 +10,7 @@ use ieee.std_logic_unsigned.all;
 
 architecture behaviour of mod60_teller is
 
-type m60_state is (clear, counting, switch);
+type m60_state is (clear, counting, switch, buff);
 
 signal state, new_state: m60_state;
 signal t_count: std_logic_vector(5 downto 0);
@@ -20,20 +20,18 @@ begin
 	count <= t_count;
 	t_clk <= t_temp;
 
-	process(clk, clk_in, sync_now, reset, state) is
+	process(clk, clk_in, sync_now, reset, state, ref, t_count) is
 	begin
 		if(reset = '1') then				-- Systeemreset
 			t_count <= "000000";
 			state <= clear;
-		elsif(reset = '0' and state = clear) then
-			t_count<= "000000";
-			state <= counting;
 		elsif(clk_in'event and clk_in = '1') then
 			state <= new_state;
-			if(state = counting) then
-				t_count <= t_count + 1;		-- Tellen gebeurt op de sturende klok
+
+			if(t_count = 59) then
+				t_count <= "000000";		-- Tellen gebeurt op de sturende klok
 			else
-				t_count <= "000000";
+				t_count <= t_count + 1;
 			end if;
 		elsif(clk'event and clk = '1') then		-- Opgaande klokflank v.d. systeemklok
 			if(sync_now = '1') then
@@ -53,23 +51,25 @@ begin
 	begin
 		case state is
 			when clear =>				-- Er wordt gereset
-				t_temp <= '1';
+				t_temp <= '0';
 				new_state <= counting;
 			when counting =>			-- Er wordt getelt
-				if(t_count = 0 or t_count = 30) then
-					t_temp <= not t_temp;
-				end if;
-				if(t_count < 58) then
-					new_state <= counting;
-				else
+				t_temp <= t_temp;
+
+				if(t_count = 58 or t_count = 28) then
 					new_state <= switch;
+				else
+					new_state <= counting;
 				end if;
 			when switch =>
 				t_temp <= t_temp;
+				new_state <= buff;
+			when buff =>
+				t_temp <= not t_temp;
 				new_state <= counting;
 			when others =>				-- Dummy state, zou nooit mogen voorkomen
 				t_temp <= '0';
 				new_state <= clear;
 		end case;
 	end process;
-end architecture behaviour;
+end behaviour;
