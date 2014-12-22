0e5c473654648608bb4d9b699d51ecf0fc590255
RensHam <renshamburger@gmail.com>
2014-12-22 14:30:35 +0100

    Kleine wijzigingen vergeten te doen

--- Wake-up light/Controller/VHDL/BCD/menu-tb-ent.vhd
+++ Wake-up light/Controller/VHDL/BCD/menu-tb-ent.vhd
@@ -1,7 +1,9 @@
+--Set the radix to hexadecimal
+
 library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity menu_tb is
-   port(uren   :out   std_logic_vector(5 downto 0);
+   port(uren   :out   std_logic_vector(5 downto 0); 
         minuten:out   std_logic_vector(6 downto 0));
 end menu_tb;
