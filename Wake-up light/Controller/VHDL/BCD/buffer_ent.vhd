f2321c99f553f883e3e967abf42e42c1da17163e
RensHam <renshamburger@gmail.com>
2015-01-05 11:09:53 +0100

    Files logichs noemen

--- Wake-up light/Controller/VHDL/BCD/buffer_ent.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-
-entity buff is
-   port(clk			:in		std_logic;
-        reset		:in		std_logic;
-		knoppen_in		:in		std_logic_vector(3 downto 0);
-		knoppen_out		:out	std_logic_vector(3 downto 0));
-end buff;
