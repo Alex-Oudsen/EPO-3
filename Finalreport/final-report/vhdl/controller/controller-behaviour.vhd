9564ec0dbee303ada452608bee5eabef95bec203
RensHam <renshamburger@gmail.com>
2015-01-08 11:24:00 +0100

    Updated vhdl toevoegen

--- Finalreport/final-report/vhdl/controller/controller-behaviour.vhd
+++ Finalreport/final-report/vhdl/controller/controller-behaviour.vhd
@@ -6,9 +6,9 @@ component menu is									-- Het blok waar het mooie en slimmen onderdelen van d
 	port(clk				:in    std_logic;
         reset		:in    std_logic;
 		knoppen		:in    std_logic_vector(3 downto 0);
-		wekdata		:in	   std_logic_vector(13 downto 0);
+		wekdata		:in	   std_logic_vector(15 downto 0);
 		enable			:out   std_logic;
-		wekker			:out		 std_logic_vector(13 downto 0);
+		wekker			:out		 std_logic_vector(15 downto 0);
 		menu_signal			:out		 std_logic_vector(2 downto 0));
 end component menu;
 
@@ -16,21 +16,21 @@ component geheugen is							-- 14 bit opslag
 	port(clk    :in    std_logic;
         reset  :in    std_logic;
         enable :in    std_logic;
-        wek_in :in    std_logic_vector(13 downto 0);
-        wek_out:out   std_logic_vector(13 downto 0));
+        wek_in :in    std_logic_vector(15 downto 0);
+        wek_out:out   std_logic_vector(15 downto 0));
 end component geheugen;
 
 
 component buff is 							--De buffer die speciaal gemaakt is voor de menu met extra eigenschappen
-	port(clk		:in		std_logic;
+	port(clk			:in		std_logic;
         reset		:in		std_logic;
-		knoppen		:in		std_logic_vector(3 downto 0);
-		knopjes		:out	std_logic_vector(3 downto 0));
+		knoppen_in		:in		std_logic_vector(3 downto 0);
+		knoppen_out		:out	std_logic_vector(3 downto 0));
 end component buff;
 
 signal knoppen_buff : std_logic_vector(3 downto 0);
 --signal menu_state 		: std_logic_vector(2 downto 0);
-signal wekdata_men,wekker_men : std_logic_vector(13 downto 0);
+signal wekdata_men,wekker_men : std_logic_vector(15 downto 0);
 signal write_enable : std_logic;
 
 begin
@@ -40,3 +40,6 @@ menory_portmap: geheugen port map (clk,reset,write_enable,wekker_men,wekdata_men
 wekker <= wekdata_men;
 
 end behaviour;
+
+
+
--- Finalreport/final-report/vhdl/controller/menu-entity.vhd
+++ Finalreport/final-report/vhdl/controller/menu-entity.vhd
@@ -6,8 +6,13 @@ entity menu is
    port(clk				:in    std_logic;
         reset		:in    std_logic;
 		knoppen		:in    std_logic_vector(3 downto 0);
-		wekdata		:in	   std_logic_vector(13 downto 0);
+		wekdata		:in	   std_logic_vector(15 downto 0);
 		enable			:out   std_logic;
-		wekker			:out		 std_logic_vector(13 downto 0);
+		wekker			:out		 std_logic_vector(15 downto 0);
 		menu_signal			:out		 std_logic_vector(2 downto 0));
 end menu;
+
+
+
+
+
