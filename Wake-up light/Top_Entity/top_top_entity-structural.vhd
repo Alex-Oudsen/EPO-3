f413355dd6c1cb5a3ba2baa9eed10d36541e53ca
MasterKeffer <gkh998@gmail.com>
2015-01-11 14:29:29 +0100

    kloten met onderdeel van roy & meer bugfixes

--- Wake-up light/Top_Entity/top_top_entity-structural.vhd
+++ Wake-up light/Top_Entity/top_top_entity-structural.vhd
@@ -73,7 +73,7 @@ component lcd_top is
 end component;
 
 --signalen
-signal clk_1hz, dcf_led, dcf_in, date_ready, beep, buzzer, led_lamp:	std_logic;
+signal clk_1hz, dcf_led, dcf_in, date_ready, beep, buzzer, led_lamp, licht_sg, lichtje:	std_logic;
 signal tijd_tijd_tijd : std_logic_vector (12 downto 0);
 signal wekkeur : std_logic_vector (15 downto 0);
 signal weekday, menu_plek : std_logic_vector (2 downto 0);
@@ -83,9 +83,9 @@ signal year		:	std_logic_vector (7 downto 0);
 
 begin
 --port maps
-	alarm_1 : alarm port map (clk, reset, clk_1hz, licht_sg, pwm_signal);
+	alarm_1 : alarm port map (clk, reset, clk_1hz, licht_sg, led_lamp);
 
-	compare_1 : compare port map (clk, reset, tijd_tijd_tijd(10 downto 7), tijd_tijd_tijd(12 downto 11), tijd_tijd_tijd(3 downto 0), tijd_tijd_tijd(6 downto 4), wekkeur(10 downto 7), wekkeur(12 downto 11), wekkeur(3 downto 0), wekkeur(6 downto 4), knoppen(4), buzzer, led_lamp);
+	compare_1 : compare port map (clk, reset, tijd_tijd_tijd(10 downto 7), tijd_tijd_tijd(12 downto 11), tijd_tijd_tijd(3 downto 0), tijd_tijd_tijd(6 downto 4), wekkeur(10 downto 7), wekkeur(12 downto 11), wekkeur(3 downto 0), wekkeur(6 downto 4), knoppen(4), buzzer, lichtje);
 
 	kontroller_1 : controller port map (clk, reset, knoppen(3 downto 0), wekkeur(15 downto 0), menu_plek);
 
@@ -94,7 +94,8 @@ begin
 	lcd_toppie : lcd_top port map (clk, reset, date_ready, tijd_tijd_tijd(12 downto 7), tijd_tijd_tijd(6 downto 0), weekday, dag_maand, month, year, dcf_led, wekkeur(15), menu_plek, wekkeur(13), wekkeur(14), clk_1hz, wekkeur(12 downto 7), wekkeur(6 downto 0), data_out, clk_out);
 
 -- AND port
-	
+	licht_sg <= (lichtje AND wekkeur(15));
+
 
 end structural;
 
