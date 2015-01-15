2751bec2f27ee89c2d04251ae85198c282bcb065
Alex <ali.x1306@gmail.com>
2015-01-16 00:44:18 +0100

    Getest - fix bevestigd
    Echter is er wel een nieuw probleempje aan het licht gekomen.

--- Layout VHDL/compare-behaviour.vhd
+++ Layout VHDL/compare-behaviour.vhd
@@ -1,68 +1,70 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-use IEEE.numeric_std.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 
 architecture behaviour of compare is
+
 	type comp_state is (steady, start, final);
 	signal state, new_state: comp_state;
 	signal alarm_uur_l, alarm_min_l: std_logic_vector(3 downto 0);
 	signal alarm_uur_h : std_logic_vector(1 downto 0);
 	signal alarm_min_h : std_logic_vector (2 downto 0);
+
 begin
-	lbl1: process (clk)
+
+	process(clk)
 	begin
-		if (clk'event and clk = '1') then
-			if (reset = '1') or (stop_alarm = '1') then
+		if(clk'event and clk = '1') then
+			if(reset = '1' or stop_alarm = '1') then
 				state <= steady;
-				alarm_min_l <= std_logic_vector(to_unsigned(0, 4));
-				alarm_min_h <= std_logic_vector(to_unsigned(0, 3));
-				alarm_uur_l <= std_logic_vector(to_unsigned(0, 4));
-				alarm_uur_h <= std_logic_vector(to_unsigned(0, 2));
 			else
-				if ((to_integer(unsigned(wekker_min_h)) = 1) and (to_integer(unsigned(wekker_min_l)) > 4)) or (to_integer(unsigned(wekker_min_h)) > 1) then
-					alarm_uur_l <= wekker_uur_l;
-					alarm_uur_h <= wekker_uur_h;
-				else
-					if (to_integer(unsigned(wekker_uur_l)) = 0) and (to_integer(unsigned(wekker_uur_h)) = 0) then
-						alarm_uur_l <= std_logic_vector(to_unsigned(3, 4));
-						alarm_uur_h <= std_logic_vector(to_unsigned(2, 2));
-					else
-						if (to_integer(unsigned(wekker_uur_l)) = 0) then
-							alarm_uur_l <= std_logic_vector(to_unsigned(9, 4));
-							alarm_uur_h <= std_logic_vector(to_unsigned(to_integer(unsigned(wekker_uur_h)) - 1 , 2));
-						else
-							alarm_uur_l <= std_logic_vector(to_unsigned(to_integer(unsigned(wekker_uur_l)) - 1 , 4));
-							alarm_uur_h <= wekker_uur_h;
-						end if;
-					end if;
-				end if;
-				if (to_integer(unsigned(wekker_min_l)) > 4) and (to_integer(unsigned(wekker_min_h)) > 0) then
-					alarm_min_l <= std_logic_vector(to_unsigned(to_integer(unsigned(wekker_min_l)) - 5 , 4));
-					alarm_min_h <= std_logic_vector(to_unsigned(to_integer(unsigned(wekker_min_h)) - 1 , 3));
-				elsif (to_integer(unsigned(wekker_min_l)) > 4) and (to_integer(unsigned(wekker_min_h)) = 0) then
-					alarm_min_l <= std_logic_vector(to_unsigned(to_integer(unsigned(wekker_min_l)) - 5 , 4));
-					alarm_min_h <= std_logic_vector(to_unsigned(5, 3));
-				elsif (to_integer(unsigned(wekker_min_l)) < 5) and (to_integer(unsigned(wekker_min_h)) = 0) then
-					alarm_min_l <= std_logic_vector(to_unsigned(5 + to_integer(unsigned(wekker_min_l)), 4));
-					alarm_min_h <= std_logic_vector(to_unsigned(4, 3));
-				elsif (to_integer(unsigned(wekker_min_l)) < 5) and (to_integer(unsigned(wekker_min_h)) = 1) then
-					alarm_min_l <= std_logic_vector(to_unsigned(5 + to_integer(unsigned(wekker_min_l)), 4));
-					alarm_min_h <= std_logic_vector(to_unsigned(5, 3));
-				else
-					alarm_min_l <= std_logic_vector(to_unsigned(5 + to_integer(unsigned(wekker_min_l)), 4));
-					alarm_min_h <= std_logic_vector(to_unsigned(to_integer(unsigned(wekker_min_h)) - 1 , 3));
-				end if;
 				state <= new_state;
 			end if;
 		end if;
 	end process;
-	lbl2: process (state, alarm_min_l, alarm_min_h, alarm_uur_l, alarm_uur_h, wekker_uur_l, wekker_uur_h, wekker_min_l, wekker_min_h, tijd_min_l, tijd_min_h, tijd_uur_l, tijd_uur_h)
+
+	process(wekker_min_h, wekker_min_l, wekker_uur_l, wekker_uur_h)
+	begin
+		if((wekker_min_h = 1 and wekker_min_l > 4) or (wekker_min_h > 1)) then
+			alarm_uur_l <= wekker_uur_l;
+			alarm_uur_h <= wekker_uur_h;
+		elsif(wekker_uur_l = 0 and wekker_uur_h = 0) then
+			alarm_uur_l <= "0011";
+			alarm_uur_h <= "10";
+		elsif(wekker_uur_l = 0) then
+			alarm_uur_l <= "1001";
+			alarm_uur_h <= wekker_uur_h - 1;
+		else
+			alarm_uur_l <= wekker_uur_l - 1;
+			alarm_uur_h <= wekker_uur_h;
+		end if;
+
+		if(wekker_min_l > 4 and wekker_min_h > 0) then
+			alarm_min_l <= wekker_min_l - 5;
+			alarm_min_h <= wekker_min_h - 1;
+		elsif(wekker_min_l > 4 and wekker_min_h = 0) then
+			alarm_min_l <= wekker_min_l - 5;
+			alarm_min_h <= "101";
+		elsif(wekker_min_l < 5 and wekker_min_h = 0) then
+			alarm_min_l <= wekker_min_l + 5;
+			alarm_min_h <= "100";
+		elsif(wekker_min_l < 5 and wekker_min_h = 1) then
+			alarm_min_l <= wekker_min_l + 5;
+			alarm_min_h <= "101";
+		else
+			alarm_min_l <= wekker_min_l + 5;
+			alarm_min_h <= wekker_min_h - 2;
+		end if;
+	end process;
+
+	process (state, alarm_min_l, alarm_min_h, alarm_uur_l, alarm_uur_h, wekker_uur_l, wekker_uur_h, wekker_min_l, wekker_min_h, tijd_min_l, tijd_min_h, tijd_uur_l, tijd_uur_h)
 	begin
 		case state is
 			when steady =>
 				geluid <= '0';
 				licht <= '0';
-				if (alarm_min_l = tijd_min_l) and (alarm_min_h = tijd_min_h) and (alarm_uur_l = tijd_uur_l) and (alarm_uur_h = tijd_uur_h) then
+				if (alarm_min_l = tijd_min_l) and (alarm_min_h = tijd_min_h) and 
+				   (alarm_uur_l = tijd_uur_l) and (alarm_uur_h = tijd_uur_h) then
 					new_state <= start;
 				else
 					new_state <= steady;
@@ -70,7 +72,8 @@ begin
 			when start =>
 				geluid <= '0';
 				licht <= '1';
-				if (wekker_uur_l = tijd_uur_l) and (wekker_uur_h = tijd_uur_h) and (wekker_min_l = tijd_min_l) and (wekker_min_h = tijd_min_h) then
+				if (wekker_uur_l = tijd_uur_l) and (wekker_uur_h = tijd_uur_h) and
+				   (wekker_min_l = tijd_min_l) and (wekker_min_h = tijd_min_h) then
 					new_state <= final;
 				else
 					new_state <= start;
@@ -86,7 +89,3 @@ begin
 		end case;
 	end process;
 end behaviour;
-
-
-
-
